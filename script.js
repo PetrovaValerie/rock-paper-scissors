8ee45e0f09c41a219e0856d455d95b10192c9629
Валерия <pristavkovaleriavladimirovna@gmail.com>
2023-03-25 20:42:20 +0300

    rearrange spaces

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ export const lose = 'Lose'.black
 export const draw = 'Draw'.grey
 const moves = process.argv.slice(2)
 export const range = Math.round(moves.length / 2)
-export const range1 = Math.round((moves.length -1) / 2)
+export const range1 = Math.round((moves.length - 1) / 2)
 const argsLen = process.argv.length
 const cmptMoves = []
 const res = []
@@ -21,43 +21,49 @@ class Game {
     constructor() {
         this.rl = readline.createInterface({
             input: process.stdin,
-            output: process.stdout})}
+            output: process.stdout
+        })
+    }
+
     start() {
-        if(new Set(process.argv).size === argsLen && argsLen % 2 !== 0 && argsLen >= 3) {
+        if (new Set(process.argv).size === argsLen && argsLen % 2 !== 0 && argsLen >= 3) {
             this.rl.question(`Available moves:\n${moves.map((move, index) =>
-            `${index + 1} - ${move}`).join('\n')}\n0 - exit\n? - help\n`, (ans) => {
-            if(parseInt(ans) === 0){
-                this.rl.close()}
-            if(ans === '?'){
-                getTable(...moves)
+                `${index + 1} - ${move}`).join('\n')}\n0 - exit\n? - help\n`, (ans) => {
+                if (parseInt(ans) === 0) {
+                    this.rl.close()
                 }
-            if(parseInt(ans) > 0 && parseInt(ans) <= moves.length){
-                let selectedMove = moves[parseInt(ans) - 1]
-                console.log(`You selected: ${selectedMove}` + '\n'+'Computer move: '+cmptChoice)
-                userMove = moves.indexOf(selectedMove)+1
-                cmptMove = moves.indexOf(cmptChoice)+1
-                genRes(userMove, cmptMove)
-                console.log('Game Result: '+res+'\n'+'HMAC key: '+uid)
-                this.rl.close()
-            }else{
-                console.log("Let's try again!")
-                this.start()}
+                if (ans === '?') {
+                    getTable(...moves)
+                }
+                if (parseInt(ans) > 0 && parseInt(ans) <= moves.length) {
+                    let selectedMove = moves[parseInt(ans) - 1]
+                    console.log(`You selected: ${selectedMove}` + '\n' + 'Computer move: ' + cmptChoice)
+                    userMove = moves.indexOf(selectedMove) + 1
+                    cmptMove = moves.indexOf(cmptChoice) + 1
+                    genRes(userMove, cmptMove)
+                    console.log('Game Result: ' + res + '\n' + 'HMAC key: ' + uid)
+                    this.rl.close()
+                } else {
+                    console.log("Let's try again!")
+                    this.start()
+                }
             })
-        }else{
-            if(moves.length === 0){
+        } else {
+            if (moves.length === 0) {
                 console.error("Error: Invalid input! You didn't enter any parameters.".green)
-            }else{
-                console.error('Error: Invalid input! Please provide an odd number of '.green+
-                                'at least 3 non-repeating strings as command line arguments.\nEx. Scissors Lizard Spock.'.green)
+            } else {
+                console.error('Error: Invalid input! Please provide an odd number of '.green +
+                    'at least 3 non-repeating strings as command line arguments.\nEx. Scissors Lizard Spock.'.green)
             }
             this.rl.close()
         }
     }
 }
+
 const game = new Game()
 game.start()
 
-for(let move in moves){
+for (let move in moves) {
     cmptMoves.push(moves[crypto.randomInt(0, moves.length)])
 }
 
@@ -65,24 +71,33 @@ const cmptChoice = cmptMoves[Math.floor(Math.random() * cmptMoves.length)]
 cmptMove = moves.indexOf(cmptChoice) + 1;
 
 setKey()
+
 function setKey() {
     const secret = new UIDGenerator(368)
     uid = secret.generateSync()
-    if(new Set(process.argv).size === argsLen && cmptChoice){
+    if (new Set(process.argv).size === argsLen && cmptChoice) {
         const hmac = crypto.createHmac("sha3-256", uid).update(cmptChoice).digest('hex')
-        console.log('HMAC: '+hmac+'\n'+'Enter your move:')
+        console.log('HMAC: ' + hmac + '\n' + 'Enter your move:')
     }
 }
 
 function genRes(move1, move2) {
-    if(move1 === move2) res.push(draw)
+    if (move1 === move2) res.push(draw)
     if (moves.length <= 5) {
-        if (move2 > move1) {(move2 - move1 <= range) ? res.push(lose) : res.push(win)}
-        if (move2 < move1) {(move1 - move2 <= range) ? res.push(win) : res.push(lose)}
+        if (move2 > move1) {
+            (move2 - move1 <= range) ? res.push(lose) : res.push(win)
+        }
+        if (move2 < move1) {
+            (move1 - move2 <= range) ? res.push(win) : res.push(lose)
+        }
     }
     if (moves.length > 5) {
-        if (move2 > move1) {(move2 - move1 <= range1) ? res.push(lose) : res.push(win)}
-        if (move2 < move1) {(move1 - move2 <= range1) ? res.push(win) : res.push(lose)}
+        if (move2 > move1) {
+            (move2 - move1 <= range1) ? res.push(lose) : res.push(win)
+        }
+        if (move2 < move1) {
+            (move1 - move2 <= range1) ? res.push(win) : res.push(lose)
+        }
     }
 }
 
--- table.js
+++ table.js
@@ -3,7 +3,7 @@ import {draw, lose, range, range1, win} from "./script.js"
 
 export function getTable(...moves) {
     const table = new Table({
-        head: ['Computer'.blue+' vs '.white+'Gamer >>', ...moves],
+        head: ['Computer'.blue + ' vs '.white + 'Gamer >>', ...moves],
         colWidths: [22]
     });
     moves.forEach((move1) => {
@@ -11,13 +11,24 @@ export function getTable(...moves) {
         moves.forEach((move2) => {
             let moveU = moves.indexOf(move1);
             let moveC = moves.indexOf(move2);
-            if(moveU === moveC) row.push(draw)
+            if (moveU === moveC) row.push(draw)
             if (moves.length <= 5) {
-                if (moveU > moveC){(moveU - moveC <= range)?row.push(lose):row.push(win)}
-                if (moveU < moveC){(moveC - moveU <= range)?row.push(win):row.push(lose)}}
-            if (moves.length > 5){
-                if (moveU > moveC){(moveU - moveC <= range1)?row.push(lose):row.push(win)}
-                if (moveU < moveC){(moveC - moveU <= range1)?row.push(win):row.push(lose)}}})
+                if (moveU > moveC) {
+                    (moveU - moveC <= range) ? row.push(lose) : row.push(win)
+                }
+                if (moveU < moveC) {
+                    (moveC - moveU <= range) ? row.push(win) : row.push(lose)
+                }
+            }
+            if (moves.length > 5) {
+                if (moveU > moveC) {
+                    (moveU - moveC <= range1) ? row.push(lose) : row.push(win)
+                }
+                if (moveU < moveC) {
+                    (moveC - moveU <= range1) ? row.push(win) : row.push(lose)
+                }
+            }
+        })
         table.push(row)
     })
     console.log(table.toString())
